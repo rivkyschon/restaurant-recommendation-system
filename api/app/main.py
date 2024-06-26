6de481e268898d69df1e5457770c7043e61cdbeb
rivkyschon <rivkyschon@outlook.co.il>
2024-06-26 09:27:32 +0300

    test infrastructure

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.routers import restaurant_router
+from app.middleware import middleware
 
 app = FastAPI()
+
+#Include middleware for logging requests
+app.add_middleware(RequestLoggerMiddleware)
 
 # Include routers
 app.include_router(restaurant_router.router)
